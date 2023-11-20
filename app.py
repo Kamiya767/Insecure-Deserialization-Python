8f9b944f55b4d12c99092b7a0756cbba2073def2
Kamiya767 <136062929+Kamiya767@users.noreply.github.com>
2023-11-21 00:54:47 +1030

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request
-import pickle, base64
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def page():
-    pickle_data = request.values.get('str')
-    return str(pickle.loads(base64.b64decode(pickle_data)))
+    json_data = request.values.get('str')
+    return str(json.loads(json_data))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
